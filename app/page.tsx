7309fe71760c81c2e964ebd3864a41d3803ce6b8
DowardCaparas <caparasdounhuward@gmail.com>
2025-04-21 21:58:11 +0800

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import ProductCategories from "./ui/product-categories";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import ProductCards from "./ui/product-card";
+import Hero from "./ui/hero";
 
 const Home = () => {
   const [search, setSearch] = useState("");
 
   return (
     <div className="lg:px-16 md:px-8 px-4 mt-10 mb-28">
+      <Hero />
         <h3 className="text-2xl text-gray-500 font-medium">Products</h3>
         <Input
           type="text"
---
+++ app/ui/hero.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section className="flex max-md:flex-col-reverse justify-between gap-10 mt-20 mb-14">
+      <div className="md:w-[50%]">
+        <h1 className="mb-2 xl:text-6xl lg:text-5xl sm:text-4xl text-2xl font-bold xl:leading-18 max-md:text-center">
+          From Daily Essentials to Hidden Gems — We&apos;ve Got It All.
+        </h1>
+        <p className="max-md:text-center">
+            Explore a curated selection of top-quality products across every
+            category. Whether you&apos;re upgrading your home, refreshing your
+            wardrobe, or looking for the perfect gift — we&apos;ve made it easy to
+            shop everything in one place.
+          </p>
+      </div>
+      <Image
+        src="/images/heroBG.webp"
+        alt="Big Cart image illustration"
+        width={500}
+        height={610}
+        className="md:w-[50%] -mt-10"
+      />
+    </section>
+  );
+};
+
+export default Hero;
